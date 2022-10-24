c0ed80089f14691465eeb1a82aa766a487c75652
0hax <0hax@protonmail.com>
2022-10-24 21:04:24 +0200

    addic7ed: check available subtitles embedded in the file.
    Signed-off-by: 0hax <0hax@protonmail.com>

--- addic7ed_async/__main__.py
+++ addic7ed_async/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+import ffmpeg
 import os
 from aiohttp import ClientSession
 from addic7ed import Addic7ed
@@ -14,10 +15,24 @@ def parse_args():
     return parser.parse_args()
 
 
+async def get_available_subtitles_from_file(tvshow):
+    streams = ffmpeg.probe(tvshow)['streams']
+    languages = list()
+    for stream in streams:
+        if stream['codec_name'] != 'subrip':
+            continue
+        languages.append(stream['tags']['language'])
+    return languages
+
+
 async def main():
     args = parse_args()
     async with ClientSession() as session:
         for tvshow in args.tvshows:
+            available_subtitles = await get_available_subtitles_from_file(tvshow)
+            if args.language in available_subtitles:
+                print('subtitle already present')
+                continue
             # TODO check if the mkv file already has fr subtitles.
             addicted = Addic7ed(session)
             guess = guessit(tvshow)
