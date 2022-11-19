29c07e98c9bbb26ecb9ab131c95bdfb9d6d3d85a
0hax <0hax@protonmail.com>
2022-11-19 09:18:00 +0100

    addic7ed: main: rework embedded subtitles check.
    Signed-off-by: 0hax <0hax@protonmail.com>

--- addic7ed_async/__main__.py
+++ addic7ed_async/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 import asyncio
-import ffmpeg
 import os
+import re
 from aiohttp_client_cache import CachedSession, FileBackend
 from addic7ed import Addic7ed
 from datetime import timedelta
@@ -12,18 +12,37 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--language', '-l', default='en',
                         help='language used to download subtitles')
+    parser.add_argument('--check-embedded-subtitles', '-c',
+                        help='check if language is already embedded in the file',
+                        default=True)
     parser.add_argument('tvshows', nargs='+')
     return parser.parse_args()
 
 
-async def get_available_subtitles_from_file(tvshow):
+async def get_sub_lang_from_file(tvshow):
+    try:
+        import ffmpeg
+    except ImportError:
+        return []
     streams = ffmpeg.probe(tvshow)['streams']
     languages = list()
     for stream in streams:
         if stream['codec_name'] != 'subrip':
             continue
+        if not stream.get('tags'):
+            continue
         languages.append(stream['tags']['language'])
     return languages
+
+
+async def embedded_langs_match(embedded_langs, requested_lang):
+    for lang in embedded_langs:
+        # Usually 2 first letters are enough to match against ISO
+        # 3166-2 standards.
+        if re.match(r"{}?".format(requested_lang[0:2]),
+                    lang, re.IGNORECASE):
+            return True
+    return False
 
 
 async def main():
@@ -34,11 +53,12 @@ async def main():
                         expire_after=timedelta(days=1))
     async with CachedSession(cache=cache) as session:
         for tvshow in args.tvshows:
-            available_subtitles = await get_available_subtitles_from_file(tvshow)
-            if args.language in available_subtitles:
-                print('subtitle already present')
-                continue
-            # TODO check if the mkv file already has fr subtitles.
+            if args.check_embedded_subtitles:
+                embedded_langs = await get_sub_lang_from_file(tvshow)
+                if await embedded_langs_match(embedded_langs, args.language):
+                    print(f'{args.language} Subtitle already present in {tvshow}')
+                    continue
+
             addicted = Addic7ed(session)
             guess = guessit(tvshow)
             # TODO check if srtfile exists already, add -f option?
